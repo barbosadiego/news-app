1f5f9c0cef3da705ef7b426d5454dca0bf99abbf
diegobarbosa <diego.gutemberg@hotmail.com>
2022-08-25 10:31:35 -0300

    initial config

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,24 @@
 </template>
 
 <style lang="scss">
+:root{
+  --bg-color: #FDFDFD;
 
+  --white: #F8F8F8;
+  --black-20: #949494;
+  --black-40: #6C6C6C;
+  --black-60: #444444;
+  --black-80: #121221;
+}
+
+*{
+  box-sizing: border-box;
+  margin: 0px;
+  padding: 0px;
+}
+
+body{
+  background-color: var(--bg-color);
+  font-family: 'Roboto', sans-serif;
+}
 </style>
