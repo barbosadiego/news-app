7bfdff9c16b0c96b72d70ede1cef72c1a6842caf
diegobarbosa <diego.gutemberg@hotmail.com>
2022-08-25 10:48:14 -0300

    app nav style

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,59 @@
 <template>
   <div id="app">
-    <nav>
+    <nav class="nav">
+      <a href="/" class="logo">
+        <img src="@/assets/logo.svg" alt="News Portal logo" />
+      </a>
+      <div class="icons">
+        <div class="search">
+          <img src="@/assets/search.svg" />
+        </div>
+        <div class="menu">
+          <img src="@/assets/menu.svg" />
+        </div>
+      </div>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-:root{
-  --bg-color: #FDFDFD;
+:root {
+  --bg-color: #fdfdfd;
 
-  --white: #F8F8F8;
+  --white: #f8f8f8;
   --black-20: #949494;
-  --black-40: #6C6C6C;
+  --black-40: #6c6c6c;
   --black-60: #444444;
   --black-80: #121221;
 }
 
-*{
+* {
   box-sizing: border-box;
   margin: 0px;
   padding: 0px;
 }
 
-body{
+body {
   background-color: var(--bg-color);
   font-family: 'Roboto', sans-serif;
+}
+
+#app {
+  padding: 30px;
+
+  .nav, .icons {
+    display: flex;
+  }
+
+  .nav{
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .icons{
+    gap: 30px;
+    cursor: pointer;
+  }
 }
 </style>
