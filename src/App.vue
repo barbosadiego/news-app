142a1da293112ba6f1662595bff04dc8a16f0bff
diegobarbosa <diego.gutemberg@hotmail.com>
2022-08-25 15:44:19 -0300

    menu animation

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,59 @@
 <template>
   <div id="app" :class="{ searchActive: searchActive }">
-    <nav class="nav">
-      <a href="/" class="logo">
-        <img src="@/assets/logo.svg" alt="News Portal logo" />
-      </a>
-      <div class="icons">
-        <div class="search">
-          <img src="@/assets/search.svg" @click="handleSearch" />
-        </div>
-        <div class="menu">
-          <img src="@/assets/menu.svg" />
-        </div>
-      </div>
-    </nav>
-
-    <transition>
-      <SearchBar v-if="searchActive" @searchActive="handleSearch"/>
+    
+    <transition name="menu">
+      <MenuItens v-if="isMenuActive"/>
     </transition>
 
-    <router-view />
+    <div class="content" :class="{'is-menu-active' : isMenuActive}">
 
-    <!-- Footer -->
+      <div class="nav">
+        <a href="/" class="logo">
+          <img src="@/assets/logo.svg" alt="News Portal logo" />
+        </a>
+        <div class="icons">
+          <div class="search" @click="handleSearch" v-if="!isMenuActive">
+            <img src="@/assets/search.svg" />
+          </div>
+          <div class="menu" @click="handleMenu">
+            <img src="@/assets/menu.svg" />
+          </div>
+        </div>
+      </div>
+
+
+      <transition>
+        <SearchBar v-if="searchActive" @searchActive="handleSearch"/>
+      </transition>
+
+      <router-view />
+
+      <!-- Footer -->
+    </div>
   </div>
 </template>
 
 <script>
 import SearchBar from '@/components/SeachModal.vue';
+import MenuItens from '@/components/MenuItens.vue';
 
 export default {
   components: {
     SearchBar,
+    MenuItens,
   },
   data() {
     return {
       searchActive: false,
+      isMenuActive: true,
     };
   },
   methods:{
     handleSearch(){
-      this.searchActive = !this.searchActive
+      this.searchActive = !this.searchActive;
+    },
+    handleMenu(){
+      this.isMenuActive = !this.isMenuActive;
     }
   }
 };
@@ -66,6 +81,14 @@ body {
   font-family: 'Roboto', sans-serif;
 }
 
+ul, li{
+  list-style: none;
+}
+
+a{
+  text-decoration: none;
+}
+
 .v-enter-active,
 .v-leave-active {
   transition: all 0.3s ease;
@@ -78,9 +101,31 @@ body {
   transform: translateY(-100px);
 }
 
+.menu-enter-active,
+.menu-leave-active {
+  transition: all 0.3s ease;
+  opacity: 1;
+}
+
+.menu-enter,
+.menu-leave-to {
+  opacity: 0;
+  transform: translateX(70%);
+}
+
 #app {
-  padding: 30px;
-  min-height: 100vh;
+  z-index: 1;
+
+  .content{
+    z-index: 5;
+    min-height: 100vh;
+    padding: 30px;
+    transition: .3s ease;
+
+    &.is-menu-active{
+      transform: translateX(-70%);
+    }
+  }
 
   .nav,
   .icons {
@@ -93,8 +138,12 @@ body {
   }
 
   .icons {
-    gap: 30px;
+    gap: 10px;
     cursor: pointer;
+
+    & > div{
+      padding: 10px;
+    }
   }
 }
 </style>
---
+++ src/components/MenuItens.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="menu">
+    <ul>
+      <li><a href="" class="link">item 1</a></li>
+      <li><a href="" class="link">item 2</a></li>
+      <li><a href="" class="link">item 3</a></li>
+      <li><a href="" class="link">item 4</a></li>
+      <li><a href="" class="link">item 5</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'MenuItens',
+}
+</script>
+
+<style lang="scss" scoped>
+.menu{
+
+  ul{
+    background-color: var(--black-80);
+    position: absolute;
+    top: 0;
+    left: 30%;
+    width: 70%;
+    height: 100vh;
+    display: flex;
+    gap: 10px;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 1rem;
+
+    li{
+      a{
+        display: block;
+        padding: 10px;
+        text-transform: uppercase;
+        font-family: 'Roboto', sans-serif;
+      }
+    }
+  }
+}
+</style>
