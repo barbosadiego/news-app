09639ab8f8b9157f0ffc09538f192815f79902e2
diegobarbosa <diego.gutemberg@hotmail.com>
2022-08-26 11:39:45 -0300

    styling home-page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,16 @@
       <h1>hot topics</h1>
       <div class="item">
         <img src="@/assets/Rectangle 1.jpg" alt="" />
-        <p class="title">
-          Massa tortor nibh nulla condimentum imperdiet scelerisque...
-        </p>
-        <span class="publishedAt">2 Hours Agoi</span>
-        <span class="source">CNN Indonesia</span>
-        <p v-if="!isMobile" class="description">Nisi, sagittis aliquet sit rutrum. Nunc, id vestibulum quam ornare adipiscing. Pellentesque sed turpis nunc gravida pharetra, sit nec vivamus pharetra. Velit, dui, egestas nisi, elementum mattis mauris, magnis. Massa tortor nibh nulla condimentum imperdiet scelerisque... read more</p>
+        <div class="text">
+          <p class="title">
+            Massa tortor nibh nulla condimentum imperdiet scelerisque...
+          </p>
+          <div class="info">
+            <span class="publishedAt">2 Hours Agoi</span>
+            <span class="source">CNN Indonesia</span>
+          </div>
+          <p v-if="!isMobile" class="description">Nisi, sagittis aliquet sit rutrum. Nunc, id vestibulum quam ornare adipiscing. Pellentesque sed turpis nunc gravida pharetra, sit nec vivamus pharetra. Velit, dui, egestas nisi, elementum mattis mauris, magnis. Massa tortor nibh nulla condimentum imperdiet scelerisque... read more</p>
+        </div>
       </div>
     </div>
 
@@ -20,44 +24,58 @@
         <div class="item">
           <img src="@/assets/Rectangle 1.jpg" alt="" />
           <h3 class="title">News Title Lorem Ipsum Dolor Sit Amet</h3>
-          <span class="publishedAt">2 Hours Agoi</span>
-          <span class="source">CNN Indonesia</span>
+          <div class="info">
+            <span class="publishedAt">2 Hours Agoi</span>
+            <span class="source">CNN Indonesia</span>
+          </div>
         </div>
         <div class="item">
           <img src="@/assets/Rectangle 1.jpg" alt="" />
           <h3 class="title">News Title Lorem Ipsum Dolor Sit Amet</h3>
-          <span class="publishedAt">2 Hours Agoi</span>
-          <span class="source">CNN Indonesia</span>
+          <div class="info">
+            <span class="publishedAt">2 Hours Agoi</span>
+            <span class="source">CNN Indonesia</span>
+          </div>
         </div>
         <div class="item">
           <img src="@/assets/Rectangle 1.jpg" alt="" />
           <h3 class="title">News Title Lorem Ipsum Dolor Sit Amet</h3>
-          <span class="publishedAt">2 Hours Agoi</span>
-          <span class="source">CNN Indonesia</span>
+          <div class="info">
+            <span class="publishedAt">2 Hours Agoi</span>
+            <span class="source">CNN Indonesia</span>
+          </div>
         </div>
         <div class="item">
           <img src="@/assets/Rectangle 1.jpg" alt="" />
           <h3 class="title">News Title Lorem Ipsum Dolor Sit Amet</h3>
-          <span class="publishedAt">2 Hours Agoi</span>
-          <span class="source">CNN Indonesia</span>
+          <div class="info">
+            <span class="publishedAt">2 Hours Agoi</span>
+            <span class="source">CNN Indonesia</span>
+          </div>
         </div>
         <div class="item">
           <img src="@/assets/Rectangle 1.jpg" alt="" />
           <h3 class="title">News Title Lorem Ipsum Dolor Sit Amet</h3>
-          <span class="publishedAt">2 Hours Agoi</span>
-          <span class="source">CNN Indonesia</span>
+          <div class="info">
+            <span class="publishedAt">2 Hours Agoi</span>
+            <span class="source">CNN Indonesia</span>
+          </div>
         </div>
         <div class="item">
           <img src="@/assets/Rectangle 1.jpg" alt="" />
           <h3 class="title">News Title Lorem Ipsum Dolor Sit Amet</h3>
-          <span class="publishedAt">2 Hours Agoi</span>
-          <span class="source">CNN Indonesia</span>
+          <div class="info">
+            <span class="publishedAt">2 Hours Agoi</span>
+            <span class="source">CNN Indonesia</span>
+          </div>
         </div>
         <div class="item">
           <img src="@/assets/Rectangle 1.jpg" alt="" />
           <h3 class="title">News Title Lorem Ipsum Dolor Sit Amet</h3>
-          <span class="publishedAt">2 Hours Agoi</span>
-          <span class="source">CNN Indonesia</span>
+          <div class="info">
+            <span class="publishedAt">2 Hours Agoi</span>
+            <span class="source">CNN Indonesia</span>
+          </div>
         </div>
       </div>
     </div>
@@ -91,17 +109,96 @@ export default {
       text-transform: capitalize;
       font-size: rem(36);
       line-height: rem(43);
+      margin-bottom: 16px;
+    }
+
+    .item{
+      position: relative;
+      &::before{
+        content: '';
+        width: 100%;
+        height: 100%;
+        display: block;
+        position: absolute;
+        border-radius: 8px;
+        // background-color: rgba($color: #000000, $alpha: .4);
+        background: linear-gradient(180deg, transparent 30%, #000000);
+      }
+
+      img{
+        width: 100%;
+        object-fit: cover;
+      }
+
+      .text{
+        position: absolute;
+        top: 0;
+        color: var(--white);
+        padding: 12px;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        justify-content: flex-end;
+
+        .title{
+          font-size: rem(16);
+          font-weight: 700;
+          line-height: rem(22);
+          font-family: 'Playfair Display', 'Times New Roman', Times, serif;
+        }
+
+        .info{
+          font-size: rem(13);
+          font-weight: 400;
+          line-height: rem(12);
+          display: flex;
+          gap: 16px;
+        }
+      }
     }
   }
 
   .latest{
 
+    h2{
+      font-size: rem(24);
+      font-weight: 700;
+      line-height: rem(29);
+      margin-bottom: 16px;
+      text-transform: capitalize;
+    }
+
     .items-array{
       display: grid;
       grid-template-columns: repeat(2, 1fr);
-      gap: 32px 20px
+      gap: 32px 20px;
+
+      .item{
+
+        img{
+          border-radius: 8px;
+          margin-bottom: 8px;
+        }
+
+        .title{
+          font-weight: 700;
+          font-size: rem(16);
+          line-height: rem(22);
+          font-family: 'Playfair Display', 'Times New Roman', Times, serif;
+        }
+
+        .info{
+          display: flex;
+          font-size: rem(10);
+          font-weight: 400;
+          line-height: rem(16);
+          gap: 5px;
+          margin-top: 16px;
+          color: var(--black-20);
+        }
+      }
     }
-    
   }
 }
 </style>
