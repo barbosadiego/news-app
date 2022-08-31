f99828f6151a5ed5a59a4ed064d0f150d1dd9952
diegobarbosa <diego.gutemberg@hotmail.com>
2022-08-31 11:51:41 -0300

    add watch for search params change

--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -1,105 +1,121 @@
 <template>
   <div class="search-results">
-    <h1>search results: </h1>
+    <h1>search results: {{ searchItem }}</h1>
     <div class="items-array">
-
-        <div class="item" v-for="item, index in searchResults" :key="`${index} - ${item.source.name}`">
-          <a :href="item.url" target="_blank" rel="noopener noreferrer">
-            <img :src="item.urlToImage ? item.urlToImage : 'https://via.placeholder.com/450'" :alt="item.title" />
-            <h3 class="title">{{ item.title }}</h3>
-            <div class="info">
-              <span class="publishedAt">{{ timeLocale(item.publishedAt) }}</span>
-              <span class="source">{{ item.source.name }}</span>
-            </div>
-          </a>
-        </div>
-
+      <div
+        class="item"
+        v-for="(item, index) in searchResults"
+        :key="`${index} - ${item.source.name}`"
+      >
+        <a :href="item.url" target="_blank" rel="noopener noreferrer">
+          <img
+            :src="
+              item.urlToImage
+                ? item.urlToImage
+                : 'https://via.placeholder.com/450'
+            "
+            :alt="item.title"
+          />
+          <h3 class="title">{{ item.title }}</h3>
+          <div class="info">
+            <span class="publishedAt">{{ timeLocale(item.publishedAt) }}</span>
+            <span class="source">{{ item.source.name }}</span>
+          </div>
+        </a>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'SearchResults',
-  data(){
-    return{
+  data() {
+    return {
       searchResults: [],
-    }
+      searchItem: '',
+    };
   },
-  methods:{
-    getParams(){
+  methods: {
+    getParams() {
       const url = new URLSearchParams(window.location.search);
       const term = url.get('query');
-      if(term){
-        this.getSearch(term)
+      this.searchItem = url.get('query');
+      if (term) {
+        this.getSearch(term);
       }
     },
-    async getSearch(query){
-      const data = await fetch(`https://newsapi.org/v2/everything?q=${query}&language=pt&apiKey=10a22d9d876f43d5976a12223845ad75`);
+    async getSearch(query) {
+      const data = await fetch(
+        `https://newsapi.org/v2/everything?q=${query}&language=pt&apiKey=10a22d9d876f43d5976a12223845ad75`,
+      );
       const response = await data.json();
       this.searchResults = response.articles;
     },
-    timeLocale(time){
-      return new Date(time).toLocaleDateString('pt-BR')
+    timeLocale(time) {
+      return new Date(time).toLocaleDateString('pt-BR');
     },
   },
-  created(){
-    this.getParams()
-  }
-}
+  created() {
+    this.getParams();
+  },
+  watch: {
+    $route: 'getParams',
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .search-results{
-    h1{
-      margin: 40px 0 1rem 0;
-      text-transform: capitalize;
+.search-results {
+  h1 {
+    margin: 40px 0 1rem 0;
+    text-transform: capitalize;
+  }
+
+  .items-array {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 1rem;
+    //tablet
+    @media screen and (min-width: 768px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+    //desktop
+    @media screen and (min-width: 1024px) {
+      grid-template-columns: repeat(4, 1fr);
     }
 
-    .items-array{
-      display: grid;
-      grid-template-columns: 1fr;
+    .item a {
+      display: flex;
+      flex-direction: column;
       gap: 1rem;
-      //tablet
-      @media screen and (min-width:768px) {
-        grid-template-columns: repeat(3,1fr);
-      }
-      //desktop
-      @media screen and (min-width:1024px){
-        grid-template-columns: repeat(4,1fr);
+
+      img {
+        height: 148px;
+        object-fit: cover;
+        object-position: top left;
+        border-radius: 8px;
+        //tablet
+        @media screen and (min-width: 768px) {
+          height: 176px;
+        }
       }
 
-      .item a{
+      .title {
+        font-family: 'Playfair Display', 'Times New Roman', Times, serif;
+        font-weight: 700;
+        font-size: rem(16);
+      }
+
+      .info {
         display: flex;
-        flex-direction: column;
-        gap: 1rem;
-
-        img{
-          height: 148px;
-          object-fit: cover;
-          object-position: top left;
-          border-radius: 8px;
-          //tablet
-          @media screen and (min-width:768px) {
-            height: 176px;
-          }
-        }
-
-        .title{
-          font-family: 'Playfair Display', 'Times New Roman', Times, serif;
-          font-weight: 700;
-          font-size: rem(16);
-        }
-
-        .info{
-          display: flex;
-          gap: 8px;
-          color: var(--black-20);
-          font-size: rem(12);
-          line-height: rem(14);
-          font-weight: 400;
-        }
+        gap: 8px;
+        color: var(--black-20);
+        font-size: rem(12);
+        line-height: rem(14);
+        font-weight: 400;
       }
     }
   }
+}
 </style>
